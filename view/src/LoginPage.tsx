5ad8a4fffdfa09fc13d52d703350d86cf1b84a8f
PeachDew <chinfurui@gmail.com>
2023-04-03 04:08:39 +0800

    -added favicon and placeholder name of app -login and create acc page successfully merged, works as intended now -also fixed some responsive issues

--- view/src/LoginPage.tsx
+++ view/src/LoginPage.tsx
@@ -9,16 +9,22 @@ import { TbMan } from "react-icons/tb";
 import { Icon } from '@chakra-ui/react'
 
 function LoginPage(props: any) {
-  const [name, setName] = useState('')
-  const [contact, setContact] = useState('')
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // login fields
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  // create acc fields
+  const [createName, setCreateName] = useState('');
+  const [createContact, setCreateContact] = useState('');
+  const [createEmail, setCreateEmail] = useState('');
+  const [createPassword, setCreatePassword] = useState('');
+
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
 
-  const handleSubmit = async (event: any) => {
+  const handleLogInSubmit = async (event: any) => {
     event.preventDefault();
-    const user = await login(email, password, props.userType);
+    const user = await login(loginEmail, loginPassword, props.userType);
     setUser(user);
     // handle the response, for example by redirecting the user to another page
     if (user) {             
@@ -29,23 +35,28 @@ function LoginPage(props: any) {
 
   const handleCreateAccountSubmit = async (event: any) => {
     event.preventDefault();
-    const success = await createUser({email, password}, props.userType);
+    console.log(createName, createContact, createEmail, createPassword, props.userType);
+    const success = await createUser({name: createName, contact: createContact, email: createEmail, password: createPassword}, props.userType);
     if (success) {
-      const user = await login(email, password, props.userType);
-      if (user) {
-        localStorage.setItem('user', JSON.stringify(user));
-        navigate("/home/"+props.userType);
-      } 
+      if (props.userType=='owner') {
+        navigate('/login/owner');
+      } else {
+        navigate('/login/customer'); 
+      }
+      setCreateName('');
+      setCreateContact('');
+      setCreateEmail('');
+      setCreatePassword('');
     }
-  }
+  };
 
   return (
     <Container className="login-container">
       <div className='homeButton'>
-          <Button leftIcon={<AiFillHome/>} w="15vw" h="3.3vw" fontSize="1.3vw"><Link to="/" className="backHome">Back to Home</Link></Button>
+          <Button leftIcon={<AiFillHome/>} w="15vw" h="3.3vw" fontSize="16px"><Link to="/" className="backHome">Back to Home</Link></Button>
       </div>
       <div className="logintext">
-          <h1 style={{fontSize:'3vw'}}>
+          <h1>
             {props.userType === "customer" ? (
               <Icon as={TbMan} />
             ) : (
@@ -57,18 +68,18 @@ function LoginPage(props: any) {
       <div className="logincreatehome-box">
         <div className="logincreate-box">
           <div className='login-box'>
-            <h1 style={{fontSize: '1.5vw'}}>Log In</h1>
+            <h1 className='login-create-text'>Log In</h1>
             <div className="login-area">
-              <Form onSubmit={handleSubmit}>
+              <Form onSubmit={handleLogInSubmit}>
                 <div className='email-pass'>
                   <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address:</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} className='formGroups'/>
+                    <Form.Control type="email" placeholder="name@example.com" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} className='formGroups'/>
                   </Form.Group>
 
                   <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className='formGroups'/>
+                    <Form.Control type="password" placeholder="Password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} className='formGroups'/>
                   </Form.Group>
                   <p></p>
                 </div>
@@ -80,28 +91,28 @@ function LoginPage(props: any) {
           </div>
 
           <div className='create-box'>
-            <h1 style={{fontSize: '1.5vw'}}>Create Account</h1>
+            <h1 className='login-create-text'>Create Account</h1>
             <div className="login-area">
-              <Form onSubmit={handleSubmit}>
+              <Form onSubmit={handleCreateAccountSubmit}>
                 <div className='email-pass'>
                 <Form.Group controlId="formBasicName">
                   <Form.Label>Name:</Form.Label>
-                  <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e) => setName(e.target.value)}  className='formGroups'/>
+                  <Form.Control type="text" placeholder="Enter name" value={createName} onChange={(e) => setCreateName(e.target.value)}  className='formGroups'/>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicContact">
                   <Form.Label>Contact:</Form.Label>
-                  <Form.Control type="text" placeholder="Enter contact" value={contact} onChange={(e) => setContact(e.target.value)} className='formGroups'/>
+                  <Form.Control type="text" placeholder="Enter contact" value={createContact} onChange={(e) => setCreateContact(e.target.value)} className='formGroups'/>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicEmailc">
                   <Form.Label>Email address:</Form.Label>
-                  <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} className='formGroups'/>
+                  <Form.Control type="email" placeholder="name@example.com" value={createEmail} onChange={(e) => setCreateEmail(e.target.value)} className='formGroups'/>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPasswordc">
                   <Form.Label>Password:</Form.Label>
-                  <Form.Control type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} className='formGroups'/>
+                  <Form.Control type="password" placeholder="Enter password" value={createPassword} onChange={(e) => setCreatePassword(e.target.value)} className='formGroups'/>
                 </Form.Group>
                 </div>
                 <Button color="#ffffff" backgroundColor={"#174b4d"} _hover={{bg: '#F5DEB3', color: '#000000'}} height="4vw" width="12vw" type="submit" fontSize="1.5vw">Create Account</Button>
@@ -109,7 +120,6 @@ function LoginPage(props: any) {
             </div>
           </div>
         </div>
-        <p className="mt-3 text-center">Don't have an account? <Link to={"/create/"+props.userType}>Create</Link></p> 
       </div>
     </Container>
   );
